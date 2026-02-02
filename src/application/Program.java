2246c5691e84bbe0f25ec1f1b894c2a22ae315d8
andrealmeida-java <andrealmeidadeveloper@gmail.com>
2026-02-02 19:41:36 -0300

    Classe implementadas: Program, Contract, Installment, ContractSevice e OnlinePaymentService. Program e ContractService ainda não estão completos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,48 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do contrato:");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.print("Data (dd/MM/yyyy): ");
+		LocalDate data = LocalDate.parse(sc.next(), fmt);
+		System.out.print("Valor do contrato: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract obj = new Contract(number, data, totalValue);
+		
+		System.out.print("Entre com o número de parcelas: ");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(null);
+		
+		contractService.processContract(obj, n);
+		
+		
+		System.out.println("Parcelas:");
+		for(Installment installment : obj.getInstallments()) {
+			System.out.println(installment);
+		}
+		
+		
+		
+		sc.close();
 	}
 
 }
